bcea3e7e783889f9b87b64173d0d9b888bab6bb0
Kevin Karma <tenngawang6080@gmail.com>
2025-04-27 21:03:00 -0400

    styling

--- components/ui/FeedbackAccordion.tsx
+++ components/ui/FeedbackAccordion.tsx
@@ -9,21 +9,21 @@ import {
 
 export const FeedbackAccordion:React.FC<FeedbackAccordionProps> = ({response}) => {
     return(
-        <Accordion type="single" collapsible className="w-full border px-3">
-      <AccordionItem value="item-1">
+        <Accordion type="single" collapsible className="w-full border border-gray-600 px-3">
+      <AccordionItem value="item-1" className="border-gray-600">
         <AccordionTrigger className="capitalize font-semibold">clarity {response?.clarity?.score}</AccordionTrigger>
         <AccordionContent>
         {response.clarity?.feedback}
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>depth {response.depth?.score}</AccordionTrigger>
+      <AccordionItem value="item-2" className="border-gray-600">
+        <AccordionTrigger className="capitalize font-semibold">depth {response.depth?.score}</AccordionTrigger>
         <AccordionContent>
         {response.depth?.feedback}
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Relevance {response.relevance?.score}</AccordionTrigger>
+      <AccordionItem value="item-3" className="border-none">
+        <AccordionTrigger className="capitalize font-semibold">Relevance {response.relevance?.score}</AccordionTrigger>
         <AccordionContent>
         {response.relevance?.feedback}
         </AccordionContent>
